060e2a1a02a9da5e85fc7d70e5ff0ef14af279eb
ArsamAliMurtaza <2017me118@student.uet.edu.pk>
2023-07-09 21:21:17 +0500

    Thumbnail added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import {Video} from "./components/Video";
+import Video from "./components/Video";
+import "./App.css";
 
 function App() {
-  return(
-    <div>
-    <div>Hello</div>
-    <Video>Video</Video>
+  return (
+    <div className="App">
+      <div>Hello</div>
+      <Video
+        title="React JS Video"
+        views="10k"
+        channel="ArsamDev"
+        time="1 year ago"
+      ></Video>
+      <Video
+        title="NextJs Video"
+        views="100k"
+        channel="ArsamDev"
+        time="2 year ago"
+      ></Video>
+      <Video
+        title="MERN Video"
+        views="10M"
+        channel="ArsamDev"
+        time="5 hours ago"
+      ></Video>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,5 +1,14 @@
-function Video() {
-    return <div>Video</div>
+import "./Video.css"
+
+function Video({title,channel,views,time}) {
+    return( 
+    <div>
+        <img src="https://loremflickr.com/160/90" alt="Katherine Johnson" />
+    <div className="title">{title}</div>
+    <div className="channel">{channel}</div>
+    <div className="views">{views} views <span>.</span> {time} </div>
+    </div>
+    );
 }
 
 export default Video;
